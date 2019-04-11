a628739c8ae9bcdc79b85e6751fe930962c40201
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-11 20:38:05 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(sg)
+  while sg > 0 
+    puts "#{sg} SECOND(S)!"
+    sg -= 1 
+  end 
+  return "HAPPY NEW YEAR!"
+  
 end
+
+
+def countdown_with_sleep(sg)
+   while sg > 0 
+    puts "#{sg} SECOND(S)!"
+    sg -= 1 
+    sleep(1)
+  end 
+  return "HAPPY NEW YEAR!"
+  
+end 
